e6c191c7bc919801b2cd25d7d5ccb4b443b94a07
alexrjgrant <alexrjgrant@hotmail.co.uk>
2020-09-02 13:24:09 +0100

    login form created
    common js file
    js renames

--- js/signUp.js
+++ js/signUp.js
@@ -410,19 +410,6 @@ function passIsValid(password)
     }
 }
 
-
-//////////////////////////////////////////////////
-//                                              //
-//          EMAIL VALIDATION CONDITIONS         //
-//                                              //
-//////////////////////////////////////////////////
-function emailIsValid(email)
-{
-    console.log(email)
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}
-
-
 //////////////////////////////////////////////////
 //                                              //
 //       CHANGE CSS BASED ON VALIDATION         //
@@ -445,99 +432,6 @@ function FP(input, FP)
         GEBID(input.id + "TC").style.color = "red";
     }
 }
-
-
-//////////////////////////////////////////////////
-//                                              //
-//         POST FIELD DATA TO PHP SCRIPT        //
-//                                              //
-//////////////////////////////////////////////////
-function postData(data, url)
-{
-    var p = new Promise((resolve, reject) =>
-    {
-        var http = new XMLHttpRequest();
-        http.open('POST', url, true);
-
-        //Send the proper header information along with the request
-        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-        http.onreadystatechange = function()
-        { //Call a function when the state changes.
-            if (http.readyState == 4 && http.status == 200)
-            {
-                //alert(http.responseText);
-                resolve(http);
-            }
-            if (http.readyState == 4 && http.status != 200)
-            {
-                //alert(JSON.parse(http.responseText).code+" : progress?!?!" + http.status);
-                reject(http);
-            }
-        }
-        http.send(objToQueryStr(data));
-
-    });
-    return p;
-}
-
-
-//////////////////////////////////////////////////
-//                                              //
-//               XHR ERROR HANDLER              //
-//                                              //
-//////////////////////////////////////////////////
-function errorHandler(error)
-{
-    var errorMsg = "";
-    
-    var rTxt     = JSON.parse(error.responseText)[0];
-    var rMsg     = rTxt.msg;
-    var rCode    = rTxt.code;
-    var httpCode = error.status;
-
-    switch (httpCode) {
-        case 400:
-          alert("400");
-          break;
-
-        case 404:
-          alert("404");
-          break;
-      }
-    
-}
-
-
-//////////////////////////////////////////////////
-//                                              //
-//        CONVERT OBJECT TO QUERY STRING        //
-//                                              //
-//////////////////////////////////////////////////
-function objToQueryStr(obj)
-{
-    var str = [];
-    for (var p in obj)
-    {
-        if (obj.hasOwnProperty(p))
-        {
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-        }
-    }
-    return str.join("&");
-}
-
-
-//////////////////////////////////////////////////
-//                                              //
-//             GET EL BY ID SHORTCUT            //
-//                                              //
-//////////////////////////////////////////////////
-function GEBID(ElID)
-{
-    return document.getElementById(ElID);
-}
-
 
 //////////////////////////////////////////////////
 //                                              //
--- login.php
+++ login.php
@@ -8,7 +8,8 @@
         <meta charset="utf-8"> <!--Set Charset-->
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script type='text/javascript' src='js/ValScript.js'></script>
+        <script type='text/javascript' src='js/login.js'></script>
+        <script type='text/javascript' src='js/common.js'></script>
         <link rel="stylesheet" type="text/css" href="css/main.css"><!--Includes CSS File-->
         <link rel="shortcut icon" type="image/x-icon" href=""> <!--Set Favicon-->
         
@@ -31,7 +32,7 @@
 
         
             <main> 
-                <div id = "SignUpForm">
+                <div id = "loginForm">
                     <form method="post" action="">
                       
                     <!--Choose Layout--> 
