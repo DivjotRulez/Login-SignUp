78a97de3a8e539a43935cc97ea7eeaf261a64574
alexrjgrant <alexrjgrant@hotmail.co.uk>
2020-09-08 11:09:59 +0100

    Password Change Update
    CSS fixed
    JS included, Functional

---
+++ js/passwordChange.js
@@ -0,0 +1,97 @@
+
+/////////////////////////////////////////////////
+// ------------------------------------------- //
+// -    THIS SCRIPT WILL:                    - //
+// -    1. SEND DATA TO BE HANDLED BY PHP    - //
+// -    2. HANDLE RETURNED ERRORS            - //
+// ------------------------------------------- //
+/////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////
+//                                              //
+//              ONLOAD INITIALISE               //
+//                                              //
+//////////////////////////////////////////////////
+function passwordChangeInit()
+{
+   
+    GEBID("btnSubmit").addEventListener("click",() => 
+    {
+            var data = {}; //DATA TO POST
+            
+            //////////////////GET INPUTS///////////////////////
+            inputs = document.getElementsByClassName("input");
+
+            ///////////////////////////////////////////////////
+            // ---------------- LOOP INPUTS ---------------- //
+            ///////////////////////////////////////////////////
+            for (var i = 0; i < inputs.length; i++)
+            {
+               data[inputs[i].id] = inputs[i].value;                
+            }
+           
+            /////////////////POST INPUTS//////////////////////
+            postData(data, 'php/passwordChange.php').then((p)=>
+            {
+                
+               if(p.status == 200)
+               {
+                   /////CONFIRM TO USER
+                //    GEBID("inputEmail").style.display = "none";
+                //    GEBID("txtBack").style.display = "none";
+                    GEBID("main").innerHTML = `
+
+                    <p id="formTitle"> Password Changed</p>
+                    <p id = "txtConf">Your Password Has Been Updated 
+                                        <br>
+                                          Please Now Login Using Your New Details 
+                    </p>
+                
+                    <form id="form">
+                        <a href = "login.php" id = "btnSubmit" >Login</a>
+                    </form>`;
+                    
+               }
+           
+            }).catch(errorHandler);
+    });
+}
+
+
+//////////////////////////////////////////////////
+//                                              //
+//               XHR ERROR HANDLER              //
+//                                              //
+//////////////////////////////////////////////////
+function errorHandler(error)
+{    
+    var rTxt     = JSON.parse(error.responseText)[0];
+
+    var rMsg     = rTxt.msg;
+    var rCode    = rTxt.code;
+    var httpCode = error.status;
+
+    switch (httpCode) {
+        case 400:
+          alert("400");
+          break;
+
+        case 404:
+          alert("404");
+          break;
+
+          case 401:
+          alert("404");
+          break;
+      }
+      
+      GEBID("txtResponse")     .style.display      = "block";
+      GEBID("txtResponse")     .innerHTML          = rMsg;
+      GEBID("inputConfirmPass").style.marginBottom = "0px";
+      GEBID("txtResponse")     .style.marginBottom = "25px";
+      GEBID("main")            .style.height       = "340px";
+      
+    
+}
+
--- passwordChange.php
+++ passwordChange.php
@@ -1,34 +1,24 @@
-<?php
-  session_start(); 
-  
-  if(!isset($_SESSION["email"]))
-  {
-    ///GO TO INDEX
-  }
-  else
-  {
-?>
 
 <html>
 
 <head>
-  <link rel="stylesheet" href="css/loginMain.css">
+  <link rel="stylesheet" href="css/passwordChangeMain.css">
   <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <script type='text/javascript' src='js/login.js'></script>
+  <script type='text/javascript' src='js/passwordChange.js'></script>
         <script type='text/javascript' src='js/common.js'></script>
   
   <title>Change Password</title>
 </head>
 
-<body onload = "changePassInit();">
+<body onload = "passwordChangeInit();">
   <main>
-    <p id="formTitle"></p>
+    <p id="formTitle">New Password</p>
     <form id="form">
       
-      <input id="inputPass"        type="password" placeholder="Password" class = "input">
-      <input id="inputConfirmPass" type="password" placeholder="Password" class = "input">
-      <input type ="hidden"  value ="<?php echo $_GET["key"]; ?>">
+      <input id="inputPass"        type="password" placeholder="Password"         class = "input">
+      <input id="inputConfirmPass" type="password" placeholder="Confirm Password" class = "input">
+      <input id="key"              type ="hidden"  class = "input"                value ="<?php echo $_GET["key"]; ?>">
       <p id = "txtResponse" style="display: none;"></p>
 
       <a id = "btnSubmit" >Change</a>
@@ -39,5 +29,3 @@
 </body>
 
 </html>
-
-  <?php } ?>
